fe1e4260349682b039f10bf92f08e7575115e892
nagaraju813 <nagarajupolasa813@gmail.com>
2025-06-19 22:21:10 +0530

    Added Perfect Squares Problem

---
+++ PerfectSquares.java
@@ -0,0 +1,23 @@
+// Time complexity: O(N * sqrt(N))
+// Space complexity: O(N)
+	
+class Solution {
+    // Time complexity: O(N * sqrt(N))
+// Space complexity: O(N)
+
+public int numSquares(int n) {
+	int[] dp = new int[n + 1];
+	dp[0] = 0;
+	
+	for (int i = 1; i <= n; i++) {
+		dp[i] = i;
+		
+		for (int j = 1; j * j <= i; j++) {
+			int square = j * j;
+			dp[i] = Math.min(dp[i], 1 + dp[i - square]);
+		}
+	}
+	
+	return dp[n];
+}
+}
